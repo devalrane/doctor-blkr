2e8c5969dca04fb8ffcb91167eb82bd888f8492a
Deval Rane <37184394+devalrane@users.noreply.github.com>
2020-05-20 20:19:00 +0530

    First Version

---
+++ blocked_domains.js
@@ -0,0 +1,12 @@
+var blocked_domains = [
+"*://*.lb.usemaxserver.de/*",
+"*://*.tracking.klickthru.com/*",
+"*://*.doubleclick.net/*",
+"*://*.googleadservices.com/*",
+"*://*.googlesyndication.com/*",
+"*://*.moat.com/*",
+"*://*.zmedia.com/*",
+"://eu.dspmulti.com",
+"://tracktrack.rdtk.io",
+"*://*.zv1.november-lax.com/*",
+"*://netpatas.com/afu.php?zoneid=1407888&var=1812939"];
